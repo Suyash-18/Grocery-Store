74540ce4b2c23783905bdf09bdef2c4111b2d7f6
Suyash <suyashrusia2005@gmail.com>
2024-05-11 21:15:17 +0530

    starting initialization done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const path = require("path");
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,"/public/css")));
+app.use(express.static(path.join(__dirname,"/public/js")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+let port = 3000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: '',
+    password: '@Suyash1234'
+});
+
+app.listen(port, () =>{
+    console.log(`App is listening on ${port}.`);
+});
