2d79f6dcd545a5401b660701df3f0c26ce2f9d1c
Suyash <suyashrusia2005@gmail.com>
2024-05-12 13:20:33 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const path = require("path");
+const { faker } = require('@faker-js/faker');
+
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,"/public/css")));
@@ -15,9 +17,32 @@ let port = 3000;
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: '',
+    database: 'grocery',
     password: '@Suyash1234'
 });
+
+let getRandomUser = () => {
+    return [faker.string.uuid(), faker.internet.userName(), faker.internet.email(), faker.phone.number(), faker.internet.password()];
+}
+let q="insert into user(id, name, email, phnumber, password) value ?;";
+let data = [];
+
+
+for(let i = 0; i < 10; i++){
+  data.push(getRandomUser());
+}
+try{
+  connection.query(q, [data], (err, result) => {
+    if (err) throw err ;
+    console.log(result); // results contains rows returned by server
+  });
+  
+}catch{
+  console.log(err);
+}
+
+connection.end();
+
 
 //Home Route
 app.get("/" , (req, res) => {
@@ -38,6 +63,11 @@ app.get("/cart" , (req, res) => {
 //Signin Route
 app.get("/signin" , (req, res) => {
     res.render("signin");
+});
+// User info route
+app.post("/user" , (req,res) => {
+    let body = req.body;
+    res.send(body);
 });
 
 app.listen(port, () =>{
